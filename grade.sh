facd3f35f4753ca55aa3d43c9b743a13f3a64659
Sam-Ham-UCSD <165727160+Sam-Ham-UCSD@users.noreply.github.com>
2024-05-07 16:07:57 -0700

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,14 +1,18 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
+
 rm -rf student-submission
 rm -rf grading-area
 
+
 mkdir grading-area
+
 
 git clone $1 student-submission
 echo 'Finished cloning'
 
-if[[-f student-submission/ListExamples.java]]; 
+
+if [[ -f student-submission/ListExamples.java ]]
 then
     echo "ListExamples.java exists"
 else
@@ -17,14 +21,33 @@ else
     exit
 fi
 
+
 cp student-submission/ListExamples.java TestListExamples.java grading-area
 cp -r lib grading-area
 
-cd grading-area
-javac -cp $CPATH ListExamples.java TestListExamples.java
-echo "The exit code of javac is $?"
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+cd grading-area
+javac -cp $CPATH *.java
+echo "The exit code of javac is $?"
+
+
+if [[ $? -ne 0 ]]
+then
+    echo "Failed to compile"
+    echo "Grade: 0"
+    exit
+fi
+
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+
+if [[ $? -ne 0 ]]
+then
+    echo "Failed at least one test"
+    echo "Grade: 1"
+    exit
+fi
+
+echo "Everything works!"
+echo "Grade: 2"
+exit
